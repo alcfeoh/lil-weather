4e55223de9b507f5103d9b1b4ac99b633da964d0
alcfeoh <alain@interstate21.com>
2025-01-22 16:49:15 -0800

    Base state added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,18 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import {provideHttpClient} from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideHttpClient(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideStore(reducers, { metaReducers }),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+]
 };
