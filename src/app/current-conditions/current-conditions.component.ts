3e34c77e89ba21a1f45b9256dc5d6c46d6c8ee5f
alcfeoh <alain@interstate21.com>
2025-01-22 17:04:04 -0800

    Display state values

--- src/app/current-conditions/current-conditions.component.ts
+++ src/app/current-conditions/current-conditions.component.ts
@@ -1,13 +1,20 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {Store} from '@ngrx/store';
+import {zipCodesFeatureKey} from '../reducers/zip-codes.reducer';
+import {AsyncPipe} from '@angular/common';
+import {State} from '../reducers';
 
 @Component({
   selector: 'app-current-conditions',
   templateUrl: './current-conditions.component.html',
   imports: [
+    AsyncPipe
   ],
   styleUrls: ['./current-conditions.component.css']
 })
 export class CurrentConditionsComponent {
 
+  zipcodes$ = inject(Store).select(zipCodesFeatureKey);
 
+  zipcodes = inject(Store).selectSignal(state => state.zipCodes);
 }
