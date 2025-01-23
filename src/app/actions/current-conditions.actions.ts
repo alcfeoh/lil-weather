b6a6f4218eaa8d4de1d6fae88fa03268ec0707ba
alcfeoh <alain@interstate21.com>
2025-01-22 17:31:31 -0800

    removed whitespace

--- src/app/actions/current-conditions.actions.ts
+++ src/app/actions/current-conditions.actions.ts
@@ -4,8 +4,8 @@ import {CurrentConditions} from '../app.types';
 export const CurrentConditionsActions = createActionGroup({
   source: 'CurrentConditions',
   events: {
-    ' CurrentConditions': emptyProps(),
-    ' CurrentConditions Loaded': props<{ zipcode: string, conditions: CurrentConditions }>(),
-    ' CurrentConditions Failed': props<{ zipcode: string, error: Error }>(),
+    'CurrentConditions': emptyProps(),
+    'CurrentConditionsLoaded': props<{ zipcode: string, conditions: CurrentConditions }>(),
+    'CurrentConditionsFailed': props<{ zipcode: string, error: Error }>(),
   }
 });
