e3fa9dd73366e5474038bfe54cdc19d9f8376df1
alcfeoh <alain@interstate21.com>
2025-01-22 18:00:53 -0800

    Changed type for CurrentConditions

--- src/app/reducers/current-conditions.reducer.ts
+++ src/app/reducers/current-conditions.reducer.ts
@@ -4,17 +4,14 @@ import {CurrentConditions} from '../app.types';
 
 export const currentConditionsFeatureKey = 'currentConditions';
 
-export type CurrentConditionsState = Map<string, CurrentConditions>;
+export type CurrentConditionsState = Record<string, CurrentConditions>;
 
 
-export const initialState: CurrentConditionsState = new Map<string, CurrentConditions>();
+export const initialState: CurrentConditionsState = {};
 
 export const currentConditionsReducer = createReducer(
   initialState,
-  on(CurrentConditionsActions.currentConditionsLoaded, (state, action) => {
-    const cc = new Map(state);
-    cc.set(action.zipcode, action.conditions);
-    return cc;
-  })
+  on(CurrentConditionsActions.currentConditionsLoaded,
+    (state, action) => ({...state, [action.zipcode]: action.conditions}))
 );
 
