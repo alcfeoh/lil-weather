ef799931d5d4b4e216b524734a139c59171c2a9b
alcfeoh <alain@interstate21.com>
2025-01-22 17:08:51 -0800

    Add support for effects

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,6 +6,7 @@ import {provideHttpClient} from '@angular/common/http';
 import { provideStore } from '@ngrx/store';
 import { reducers, metaReducers } from './reducers';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,6 +14,7 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     provideRouter(routes),
     provideStore(reducers, { metaReducers }),
-    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+    provideEffects()
 ]
 };
